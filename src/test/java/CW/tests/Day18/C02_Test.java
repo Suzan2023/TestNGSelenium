38b2de9ce0eb1e4830304e3489a130832824ea1e
Suzan Goger <suzangoger@gmail.com>
2023-05-11 08:12:07 +0100

    classes added

---
+++ src/test/java/CW/tests/Day18/C02_Test.java
@@ -0,0 +1,21 @@
+package CW.tests.Day18;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(C01_Listener.class)
+public class C02_Test {
+
+
+    @Test
+    public void successfullTest(){
+
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void failedTest(){
+        Assert.assertEquals(1,2);
+    }
+}
